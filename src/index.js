349a7be46acc6a392796da08e04510ef7200e682
pajgon <43402426+pajgon@users.noreply.github.com>
2018-10-31 11:09:19 +0100

    SCRIPTS
    nefunguje

--- src/index.js
+++ src/index.js
@@ -10,3 +10,25 @@ function updateDisplay() {
 }
 
 updateDisplay();
+
+function inputDigit(digit) {
+  if (calculator.displayValue == "0") calculator.displayValue = digit;
+  else calculator.displayValue = calculator.displayValue + digit;
+}
+
+const keys = document.querySelector(".calculator-keys");
+keys.addEventListener("click", event => {
+  const { target } = event;
+  if (!target.matches("button")) {
+    return;
+  }
+  if (target.classList.contains("operator")) {
+    return;
+  }
+  if (target.classList.contains("all-clear")) {
+    return;
+  }
+
+  inputDigit(target.value);
+  updateDisplay();
+});
